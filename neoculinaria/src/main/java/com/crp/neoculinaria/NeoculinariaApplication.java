5f94e8ddd667c6cca70abc393ce40491c7b3cee8
Mateus Ribeiro <mateusrfeijo@gmail.com>
2025-05-22 16:48:09 -0300

    Iniciado projeto em backend Java

---
+++ neoculinaria/src/main/java/com/crp/neoculinaria/NeoculinariaApplication.java
@@ -0,0 +1,12 @@
+package com.crp.neoculinaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeoculinariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeoculinariaApplication.class, args);
+	}
+}
---
+++ neoculinaria/src/test/java/com/crp/neoculinaria/NeoculinariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crp.neoculinaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeoculinariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
