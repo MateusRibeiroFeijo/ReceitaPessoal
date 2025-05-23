e11dfe21937ef62be3384d2d1b06a3e412f9fece
Mateus Ribeiro <mateusrfeijo@gmail.com>
2025-05-22 21:31:45 -0300

    Cadastro de receitas funcionando!

--- neoculinaria/src/main/java/com/crp/neoculinaria/controller/ReceitaController.java
+++ neoculinaria/src/main/java/com/crp/neoculinaria/controller/ReceitaController.java
@@ -4,7 +4,10 @@ import com.crp.neoculinaria.model.Receita;
 import com.crp.neoculinaria.service.ReceitaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ReceitaController {
@@ -17,8 +20,15 @@ public class ReceitaController {
     }
     
     @GetMapping("/cadastro")
-    public String exibirFormulario(){
+    public String exibirFormulario(Model model){
+        model.addAttribute("receita", new Receita());
         return "receita-cadastro";
+    }
+    
+    @PostMapping("/gravar")
+    public String processarFormulario(@ModelAttribute Receita receita){
+        receitaService.salvar(receita);
+        return "redirect:/lista";
     }
     
     @GetMapping("/lista")
