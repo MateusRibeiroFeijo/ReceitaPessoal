bc37cb7ab2a18c7cec08414c869b63e18c174136
Mateus Ribeiro <mateusrfeijo@gmail.com>
2025-05-22 22:04:31 -0300

    Excluir e editar receita funcionando!

--- neoculinaria/src/main/java/com/crp/neoculinaria/controller/ReceitaController.java
+++ neoculinaria/src/main/java/com/crp/neoculinaria/controller/ReceitaController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -35,5 +36,17 @@ public class ReceitaController {
     public String lista(Model model){
         model.addAttribute("receitas", receitaService.listarTodos());
         return "receita-listagem";
+    }
+    
+    @GetMapping("/excluir/{id}")
+    public String excluir(@PathVariable int id){
+        receitaService.excluir(id);
+        return "redirect:/lista";
+    }
+    
+    @GetMapping("/alterar/{id}")
+    public String alterar(@PathVariable int id, Model model){
+        model.addAttribute("receita", receitaService.buscarPorId(id));
+        return "receita-cadastro";
     }
 }
